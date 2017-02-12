6475f79a8ad54fd4bbf6d4fcd54dc24a01b2806b
Rocky Demoff <rocky@airmap.com>
2017-02-12 10:06:02 -0800

    Swift 3 Upgrade

--- ALSpotlightView/ViewController.swift
+++ ALSpotlightView/ViewController.swift
@@ -11,14 +11,14 @@ class ViewController: UIViewController {
     //MARK- View lifecycle
     
     override func loadView() {
-        self.view = UIView(frame: UIScreen.mainScreen().bounds)
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view = UIView(frame: UIScreen.main.bounds)
+        self.view.backgroundColor = UIColor.white
         
         let label = UILabel(frame: self.view.bounds)
-        label.textAlignment = .Center
-        label.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        label.textAlignment = .center
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         label.numberOfLines = 0
-        label.lineBreakMode = .ByWordWrapping
+        label.lineBreakMode = .byWordWrapping
         label.text = "Tap Anywhere in screen\nto see Spotlight View"
         self.view.addSubview(label)
     }
@@ -26,16 +26,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "onTap:")
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.onTap(_:)))
         tapGestureRecognizer.numberOfTapsRequired = 1
         self.view.addGestureRecognizer(tapGestureRecognizer)
     }
     
     //MARK- Tap Gesture Recognizer
     
-    func onTap(sender: UITapGestureRecognizer) {
-        let location = sender.locationInView(self.view)
-        var spotlightCenter = CGPointZero
+    func onTap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: self.view)
+        var spotlightCenter = CGPoint.zero
         spotlightCenter.x = location.x / self.view.bounds.size.width
         spotlightCenter.y = location.y / self.view.bounds.size.height
         
